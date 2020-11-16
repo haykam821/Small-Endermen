2e1ff3910064ef35aa057d330185ed773d27db78
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-11-16 09:54:18 -0500

    Add tiny ender pearls

--- src/main/java/io/github/haykam821/smallendermen/item/ModItems.java
+++ src/main/java/io/github/haykam821/smallendermen/item/ModItems.java
@@ -9,6 +9,7 @@ import net.minecraft.util.registry.Registry;
 
 public enum ModItems {
 	SMALL_ENDER_PEARL("small_ender_pearl", new CustomEnderPearlItem(10, 0.4f, 1, new Item.Settings().maxCount(32).group(ItemGroup.MISC))),
+	TINY_ENDER_PEARL("tiny_ender_pearl", new CustomEnderPearlItem(5, 0.8f, 0.5f, new Item.Settings().group(ItemGroup.MISC))),
 	SMALL_ENDERMAN_SPAWN_EGG("small_enderman_spawn_egg", new SpawnEggItem(Main.SMALL_ENDERMAN, 0x2C2C2C, 0x1D1D1D, new Item.Settings().group(ItemGroup.MISC))),
 	TINY_ENDERMAN_SPAWN_EGG("tiny_enderman_spawn_egg", new SpawnEggItem(Main.TINY_ENDERMAN, 0x3C3C3C, 0x2C2C2C, new Item.Settings().group(ItemGroup.MISC)));
 
